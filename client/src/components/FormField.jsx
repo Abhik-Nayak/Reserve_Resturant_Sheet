d392f059ef284d6b3a4fc93090b136b2a8ef75b7
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2023-04-08 13:20:04 +0530

    Abhuk//08-04-23

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,35 +1,84 @@
-import React from 'react';
+import React, { useEffect ,useState} from 'react';
 import { Link } from 'react-router-dom';
 import user from "../assets/webdeveloper.svg";
 
 const FormField = () => {
-  return (
-    <section className=" mt-20 min-h-screen flex items-center justify-center">
+  const [status,setStatus] = useState("");
+  let strength;
+  const strengthlabels = ["weak", "medium", "strong"];
+  const getStrength = (val) => {
+    let password = "";
+    password = val;
+    let strengthIndicator = -1;
+    let lower = false,
+      upper = false,
+      symbols = false,
+      numbers = false;
+    for (let i = 0; i < password.length; i++) {
+      const char = password.charCodeAt(i);
+      // if (!upper && char >= 65 && char <= 90) {
+      //   upper = true;
+      //   strengthIndicator++;
+      // }
+      if (!lower && char >= 97 && char <= 122) {
+        lower = true;
+        strengthIndicator++;
+      }
+      if (!numbers && char >= 48 && char <= 57) {
+        numbers = true;
+        strengthIndicator++;
+      }
+      if (!symbols && char >= 33 && char <= 47) {
+        symbols = true;
+        strengthIndicator++;
+      }
+    }
+    console.log(strengthlabels[strengthIndicator]);
+    strength = strengthlabels[strengthIndicator];
+    if (strength === "weak") {
+      setStatus("bg-[#e24c71] w-1/3")
+    }
+    if (strength === "medium") {
+      setStatus("bg-[#f39845] w-2/3")
+    }
+    if (strength === "strong") {
+      setStatus("bg-57c558 w-3/3")
+    }
+  }
+
+const handleChange = (e) => {
+  getStrength(e.target.value);
+}
+return (
+  <section className=" mt-20 min-h-screen flex items-center justify-center">
     {/* <!-- login container --> */}
     <div className="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
       {/* <!-- form --> */}
       <div className="md:w-1/2 px-8 md:px-16">
         <h2 className="font-bold text-2xl text-blue-500">Login</h2>
         <p className="text-xs mt-4 text-blue-500">If you are already a member, easily log in</p>
-  
+
         <form action="" className="flex flex-col gap-4">
-          <input className="p-2 mt-8 rounded-xl border" type="email" name="email" placeholder="Email"/>
+          <input className="p-2 mt-8 rounded-xl border" type="email" name="email" placeholder="Email" />
           <div className="relative">
-            <input className="p-2 rounded-xl border w-full" type="password" name="password" placeholder="Password"/>
+            <input className="p-2 rounded-xl border w-full" type="password" name="password" placeholder="Password" onChange={handleChange} />
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" className="bi bi-eye absolute top-1/2 right-3 -translate-y-1/2" viewBox="0 0 16 16">
               <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
               <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
             </svg>
           </div>
+          <div className='mx-2 flex-auto flex items-center gap-2 h-2 rounded bg-[#dfe1f0] '>
+            <div className={`h-2 rounded w-0  ${status}`}></div>
+          </div>
           <button className="bg-blue-500 rounded-xl text-white py-2 hover:scale-105 duration-300">Login</button>
         </form>
-  
+
         <div className="mt-6 grid grid-cols-3 items-center text-gray-400">
-          <hr className="border-gray-400"/>
+          <hr className="border-gray-400" />
           <p className="text-center text-sm">OR</p>
-          <hr className="border-gray-400"/>
+          <hr className="border-gray-400" />
         </div>
-  
+
         <button className="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 text-blue-500">
           <svg className="mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="25px">
             <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z" />
@@ -39,24 +88,24 @@ const FormField = () => {
           </svg>
           Login with Google
         </button>
-  
+
         <div className="mt-5 text-xs border-b border-blue-500 py-4 text-blue-500">
           <Link to="/">Forgot your password?</Link>
         </div>
-  
+
         <div className="mt-3 text-xs flex justify-between items-center text-blue-500">
           <p>Don't have an account?</p>
           <button className="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300"><Link to="/signup">Register</Link></button>
         </div>
       </div>
-  
+
       {/* <!-- image --> */}
       <div className="md:block hidden w-1/2">
-        <img className="rounded-2xl " src={user} alt="img"/>
+        <img className="rounded-2xl " src={user} alt="img" />
       </div>
     </div>
   </section>
-  )
+)
 }
 
 export default FormField
