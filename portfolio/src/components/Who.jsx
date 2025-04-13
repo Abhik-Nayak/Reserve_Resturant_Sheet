a1ae01a04fa364d6d74403de3078c53b47445462
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2025-04-13 07:30:03 +0530

    code push

--- portfolio/src/components/Who.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Who = () => {
-  return (
-    <div className='h-screen  snap-center'>Who</div>
-  )
-}
-
-export default Who
--- portfolio/src/components/Works.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Works = () => {
-  return (
-    <div className='h-screen  snap-center'>Works</div>
-  )
-}
-
-export default Works
